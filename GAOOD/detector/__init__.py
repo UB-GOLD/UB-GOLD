16a66db78929d3e6ed744ac743df7c280736fb3a
YL-wang <43315994+YL-wang@users.noreply.github.com>
2024-05-02 20:15:59 +1000

    Update __init__.py

--- GAOOD/detector/__init__.py
+++ GAOOD/detector/__init__.py
@@ -5,7 +5,9 @@ from .mybase import DeepDetector
 from .GOOD_D import GOOD_D
 from .GLocalKD import GLocalKD
 from .GraphDE import GraphDE
+from .GLADC import GLADC
+from .SIGNET import SIGNET
 
 __all__ = [
-    "Detector", "DeepDetector", "GOOD_D","GraphDE","GLocalKD"
+    "Detector", "DeepDetector", "GOOD_D","GraphDE","GLocalKD", "GLADC","SIGNET"
 ]
