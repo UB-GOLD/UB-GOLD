edf061437ff7a52e69c99e2f76974859546737a4
YL-wang <43315994+YL-wang@users.noreply.github.com>
2024-05-07 22:32:56 +1000

    Update utils.py

--- benchmark/utils.py
+++ benchmark/utils.py
@@ -6,39 +6,41 @@ from sklearn.ensemble import IsolationForest
 
 
 def init_model(args):
-   
+    weight_decay = 0.01
     model_name = args.model
-    gpu = args.gpu
-
+   
     
     if model_name == "GOOD-D":
-        return GOOD_D(args.dataset_num_features,args.hidden_dim, args.num_layer, args.dg_dim+args.rw_dim,
+        return GOOD_D(in_dim = args.dataset_num_features, hid_dim = args.hidden_dim, 
+                      num_layers = args.num_layer,
+                      str_dim = args.dg_dim+args.rw_dim,
                       weight_decay=weight_decay,
-                     dropout=0.5,
+                     dropout=args.dropout,
                      lr=args.lr,
-                     epoch=epoch,
-                     gpu=gpu,
-                     batch_size=args.batch_size,
-                     num_neigh=num_neigh)
+                     epoch=args.num_epoch,
+                     gpu=args.gpu,
+                     batch_size=args.batch_size)
     
     elif model_name == "GraphDE":
         
-        return GraphDE(in_dim=args.dataset_num_features,hid_dim=args.hidden_dim,
+        return GraphDE(in_dim=args.dataset_num_features,
+                       hid_dim=args.hidden_dim,
                        num_layers=args.num_layer,
                        str_dim=args.dg_dim+args.rw_dim,
                        weight_decay=weight_decay,
-                     dropout=0.5,
+                     dropout=args.dropout,
                      lr=args.lr,
                      n_train_data = args.n_train,
-                     epoch=epoch,
-                     gpu=gpu,
+                     epoch=args.num_epoch,
+                     gpu=args.gpu,
                      batch_size=args.batch_size,
-                     num_neigh=num_neigh,grand=False)
+                     grand=False)
 
     elif model_name == "GLocalKD":
 
         return GLocalKD(in_dim=args.dataset_num_features,
-                        max_nodes_num=args.max_nodes_num)
+                        max_nodes_num=args.max_nodes_num,
+                       num_epochs=args.num_epoch)
        
     elif model_name == "GLADC":
         return GLADC(lr=args.lr,
