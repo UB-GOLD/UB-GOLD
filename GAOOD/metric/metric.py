64fba87a29b93f2c3c70f94064ae061793e9da9a
YL-wang <43315994+YL-wang@users.noreply.github.com>
2024-05-09 14:22:37 +1000

    Update metric.py

--- GAOOD/metric/metric.py
+++ GAOOD/metric/metric.py
@@ -5,6 +5,7 @@ from sklearn.metrics import (
 )
 
 import numpy as np
+from sklearn.metrics import roc_curve
 
 def ood_auc(label,socre):
     return roc_auc_score(label, socre)
@@ -12,44 +13,12 @@ def ood_auc(label,socre):
 def ood_aupr(label,score):
     return average_precision_score(label,score)
 
-def fpr_and_fdr_at_recall(y_true, y_score, recall_level=0.95, pos_label=None):
-    classes = np.unique(y_true)
-    if (pos_label is None and
-            not (np.array_equal(classes, [0, 1]) or
-                     np.array_equal(classes, [-1, 1]) or
-                     np.array_equal(classes, [0]) or
-                     np.array_equal(classes, [-1]) or
-                     np.array_equal(classes, [1]))):
-        raise ValueError("Data is not binary and pos_label is not specified")
-    elif pos_label is None:
-        pos_label = 1.
+def fpr95(y_true, y_scores):
+    # 计算ROC曲线的值
+    fpr, tpr, thresholds = roc_curve(y_true, y_scores)
 
-    # make y_true a boolean vector
-    y_true = (y_true == pos_label)
+    # 寻找第一个tpr >= 0.95的索引
+    target = np.where(tpr >= 0.95)[0][0]
 
-    # sort scores and corresponding truth values
-    desc_score_indices = np.argsort(y_score, kind="mergesort")[::-1]
-    y_score = y_score[desc_score_indices]
-    y_true = y_true[desc_score_indices]
-
-    # y_score typically has many tied values. Here we extract
-    # the indices associated with the distinct values. We also
-    # concatenate a value for the end of the curve.
-    distinct_value_indices = np.where(np.diff(y_score))[0]
-    threshold_idxs = np.r_[distinct_value_indices, y_true.size - 1]
-
-    # accumulate the true positives with decreasing threshold
-    tps = stable_cumsum(y_true)[threshold_idxs]
-    fps = 1 + threshold_idxs - tps      # add one because of zero-based indexing
-
-    thresholds = y_score[threshold_idxs]
-
-    recall = tps / tps[-1]
-
-    last_ind = tps.searchsorted(tps[-1])
-    sl = slice(last_ind, None, -1)      # [last_ind::-1]
-    recall, fps, tps, thresholds = np.r_[recall[sl], 1], np.r_[fps[sl], 0], np.r_[tps[sl], 0], thresholds[sl]
-
-    cutoff = np.argmin(np.abs(recall - recall_level))
-
-    return fps[cutoff] / (np.sum(np.logical_not(y_true)))   # fps[cutoff]/(fps[cutoff] + tps[cutoff])
+    # 返回该点的FPR
+    return fpr[target]
