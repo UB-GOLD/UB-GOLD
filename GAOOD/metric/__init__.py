9424b62c36febd8fb3c0be3b7e895d1c2ef4680a
YL-wang <43315994+YL-wang@users.noreply.github.com>
2024-05-09 13:00:59 +1000

    Update __init__.py

--- GAOOD/metric/__init__.py
+++ GAOOD/metric/__init__.py
@@ -1,3 +1,3 @@
-from .metric import ood_auc, ood_aupr
+from .metric import ood_auc, ood_aupr,fpr_and_fdr_at_recall
 
-__all__ = ['ood_auc','ood_aupr']
+__all__ = ['ood_auc','ood_aupr','fpr_and_fdr_at_recall']
