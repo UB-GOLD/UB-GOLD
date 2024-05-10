17f80c3c350de5f65248a1e778de72169602b0e6
YL-wang <43315994+YL-wang@users.noreply.github.com>
2024-05-10 10:36:26 +1000

    Update GraphDE.py

--- GAOOD/detector/GraphDE.py
+++ GAOOD/detector/GraphDE.py
@@ -10,68 +10,6 @@ import numpy as np
 from GAOOD.metric import *
 import os
 
-
-def run_kmeans(x, args):
-    results = {}
-
-    d = x.shape[1]
-    k = args.num_cluster
-    clus = faiss.Clustering(d, k)
-    clus.niter = 20
-    clus.nredo = 5
-    clus.seed = 0
-    clus.max_points_per_centroid = 1000
-    clus.min_points_per_centroid = 3
-
-    res = faiss.StandardGpuResources()
-    cfg = faiss.GpuIndexFlatConfig()
-    cfg.useFloat16 = False
-
-    try:
-        index = faiss.GpuIndexFlatL2(res, d, cfg)
-        clus.train(x, index)
-    except:
-        print('Fail to cluster with GPU. Try CPU...')
-        index = faiss.IndexFlatL2(d)
-        clus.train(x, index)
-
-    D, I = index.search(x, 1)
-    im2cluster = [int(n[0]) for n in I]
-
-    centroids = faiss.vector_to_array(clus.centroids).reshape(k, d)
-
-    Dcluster = [[] for c in range(k)]
-    for im, i in enumerate(im2cluster):
-        Dcluster[i].append(D[im][0])
-
-    density = np.zeros(k)
-    for i, dist in enumerate(Dcluster):
-        if len(dist) > 1:
-            d = (np.asarray(dist) ** 0.5).mean() / np.log(len(dist) + 10)
-            density[i] = d
-
-    dmax = density.max()
-    for i, dist in enumerate(Dcluster):
-        if len(dist) <= 1:
-            density[i] = dmax
-
-    density = density.clip(np.percentile(density, 30),
-                           np.percentile(density, 70))
-    density = density / density.mean() + 0.5
-
-    centroids = torch.Tensor(centroids).cuda()
-    centroids = torch.nn.functional.normalize(centroids, p=2, dim=1)
-
-    im2cluster = torch.LongTensor(im2cluster).cuda()
-    density = torch.Tensor(density).cuda()
-
-    results['centroids'] = centroids
-    results['density'] = density
-    results['im2cluster'] = im2cluster
-
-    return results
-
-
 class GraphDE(DeepDetector):
   
 
