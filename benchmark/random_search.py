ef8b1dc0a0b0501b14009f16ebe1e7b9523c375d
YL-wang <43315994+YL-wang@users.noreply.github.com>
2024-05-10 10:57:23 +1000

    Update random_search.py

--- benchmark/random_search.py
+++ benchmark/random_search.py
@@ -1,16 +1,75 @@
-import argparse
-import time
-import pandas
-from copy import deepcopy
-from search_utils import *
-import warnings
-warnings.filterwarnings("ignore")
-seed_list = list(range(3407, 10000, 10))
-
-import os
-import numpy as np
+import tqdm
 import torch
-import random
+import argparse
+import warnings
+import sys, os
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(base_dir)
+from GAOOD.metric import *
+from utils import init_model
+from dataloader.data_loader import *
+import pandas as pd
+import statistics
+'''
+python benchmark/mymain.py -exp_type oodd -DS_pair BZR+COX2 -num_epoch 400 -num_cluster 2 -alpha 0
+oodd（两个数据集OOD），ood:是GOOD/Drugood，ad :异常检测（tox/TU）
+model：模型名字
+DS_pair BZR+COX2  对应两个数据集的OOD
+Ds, 剩下两个，ood和ad
+超参
+
+'''
+def save_results_csv(model_result, model_name):
+    # 指定结果文件夹和文件名
+    results_dir = 'results'
+    filename = f'{results_dir}/{model_name}.csv'
+    
+    # 确保结果文件夹存在
+    if not os.path.exists(results_dir):
+        os.mkdir(results_dir)
+    
+    # 将字典转换为DataFrame
+    df = pd.DataFrame([model_result])
+    
+    # 检查文件是否存在来决定是否写入表头
+    if os.path.exists(filename):
+        df.to_csv(filename, mode='a', header=False, index=False)
+    else:
+        df.to_csv(filename, mode='w', header=True, index=False)
+
+    print(f'Saved results to {filename}')
+    
+def process_model_results(auc, ap, rec, args):
+    auc_final = sum(auc) / len(auc)
+    ap_final = sum(ap) / len(ap)
+    rec_final = sum(rec) / len(rec)
+    auc_variance = statistics.variance(auc)
+    ap_variance = statistics.variance(ap)
+    rec_variance = statistics.variance(rec)
+
+    model_result = {}
+    file_id = args.model  # 使用模型名称作为文件标识
+    
+    # 根据不同的实验类型添加数据
+    if args.exp_type == 'oodd':
+        key_prefix = args.DS_pair
+    else:
+        key_prefix = args.DS
+    
+    # 格式化数据并添加到结果字典中
+    model_result['Dataset'] = key_prefix
+    model_result['AUROC'] = f"{auc_final * 100:.2f}%"
+    model_result['AUROC_Var'] = f"{auc_variance * 100:.2f}%"
+    model_result['AUPRC'] = f"{ap_final * 100:.2f}%"
+    model_result['AUPRC_Var'] = f"{ap_variance * 100:.2f}%"
+    model_result['FPR95'] = f"{rec_final * 100:.2f}%"
+    model_result['FPR95_Var'] = f"{rec_variance * 100:.2f}%"
+
+    save_results_csv(model_result, file_id)
+
+
+
+
 def set_seed(seed=3407):
     os.environ['PYTHONHASHSEED'] = str(seed)
     random.seed(seed)
@@ -21,94 +80,140 @@ def set_seed(seed=3407):
         torch.cuda.manual_seed_all(seed)
         torch.backends.cudnn.deterministic = True
 
+def main(args):
+    auc, ap, rec = [], [], []
+    model_result = {'name': args.model}
+    
+    set_seed()
+    # import ipdb
+    # ipdb.set_trace()
+    
+    for _ in tqdm.tqdm(range(args.num_trial)):
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--trials', type=int, default=100)
-parser.add_argument('--semi_supervised', type=int, default=0)
-parser.add_argument('--models', type=str, default=None)
-parser.add_argument('--datasets', type=str, default=None)
-parser.add_argument('--device', type=str, default='cuda')
+        if args.exp_type == 'ad':
+            print("-------")
+            print(args.exp_type)
+            if args.DS.startswith('Tox21'):
+                dataset_train, dataset_val, dataset_test, dataloader, dataloader_val, dataloader_test, meta = get_ad_dataset_Tox21(args)
+            else:
+                splits = get_ad_split_TU(args, fold=args.num_trial)
+        if args.exp_type == 'oodd':
+            print("-------")
+            print(args.exp_type)
+            dataset_train, dataset_val, dataset_test, dataloader, dataloader_val, dataloader_test, meta = get_ood_dataset(args)
+        elif args.exp_type == 'ad' and not args.DS.startswith('Tox21'):
+            print("-------")
+            print(args.exp_type)
+            dataset_train, dataset_val, dataset_test, dataloader, dataloader_val, dataloader_test, meta = get_ad_dataset_TU(args, splits[_])
+        elif args.exp_type == 'ood':
+            print("-------")
+            print(args.exp_type)
+            dataset_train, dataset_val, dataset_test, dataloader, dataloader_val, dataloader_test, meta = get_ood_dataset_spilt(args)
+            
 
-args = parser.parse_args()
+        
+
+        args.max_nodes_num = meta['max_nodes_num']
+        args.dataset_num_features = meta['num_feat']
+        args.n_train =  meta['num_train']
+        args.n_edge_feat = meta['num_edge_feat']
+
+        model = init_model(args)
+        ###如果要自定义dataloader,就把dataset传进去，dataloader=None,否则按下面的来即可
+        
+        if args.model == 'GOOD-D':
+            print(args.model)
+            model.fit(dataset=dataset_train, args=args, label=None, dataloader=dataloader, dataloader_val=dataloader_val)
+        elif args.model == 'GraphDE':
+            print(args.model)
+            model.fit(dataset=dataset_train, args=args, label=None, dataloader=dataloader, dataloader_val=dataloader_val)
+        elif args.model == 'GLocalKD':
+            print(args.model)
+            model.fit(dataset=dataset_train, args=args, label=None, dataloader=dataloader, dataloader_val=dataloader_val)
+        elif args.model == 'GLADC':
+            print(args.model)
+            model.fit(dataset=dataset_train, args=args, label=None, dataloader=dataloader, dataloader_val=dataloader_val)
+        elif args.model == 'SIGNET':
+            print(args.model)
+            model.fit(dataset=dataset_train, args=args, label=None, dataloader=dataloader, dataloader_val=dataloader_val)
+        elif args.model == 'CVTGAD':
+            print(args.model)
+            model.fit(dataset=dataset_train, args=args, label=None, dataloader=dataloader, dataloader_val=dataloader_val)
+        else:
+            model.fit(dataset_train)
+
+        score, y_all = model.predict(dataset=dataset_test, dataloader=dataloader_test, args=args, return_score=False)
+        
+        rec.append(fpr95(y_all, score))
+        auc.append(ood_auc(y_all, score))
+        ap.append(ood_aupr(y_all, score))
+        print("AUROC:", auc[-1])
+        print("AUPRC:", ap[-1])
+        print("FPR95:", rec[-1])
+        
+    process_model_results(auc, ap, rec, args)
 
 
-columns = ['name']
-new_row = {}
-datasets = ['reddit', 'weibo', 'amazon', 'yelp', 'tfinance',
-            'elliptic', 'tolokers', 'questions', 'dgraphfin', 'tsocial']
-models = model_detector_dict.keys()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-model", type=str, default="GOOD-D",
+                        help="supported model: [GLocalKD, GLADC, SIGNET, GOOD-D, GraphDE, CVTGAD]."
+                             "Default: GLADC")
+    parser.add_argument("-gpu", type=int, default=0,
+                        help="GPU Index. Default: -1, using CPU.")
 
-if args.datasets is not None:
-    if '-' in args.datasets:
-        st, ed = args.datasets.split('-')
-        datasets = datasets[int(st):int(ed)+1]
-    else:
-        datasets = [datasets[int(t)] for t in args.datasets.split(',')]
-    print('Evaluated Datasets: ', datasets)
+    parser.add_argument('-exp_type', type=str, default='ad', choices=['oodd', 'ad','ood'])
+    parser.add_argument('-DS', help='Dataset', default='DHFR') 
+    parser.add_argument('-DS_ood', help='Dataset', default='ogbg-molsider')
+    parser.add_argument('-DS_pair', default=None)
+    parser.add_argument('-rw_dim', type=int, default=16)
+    parser.add_argument('-dg_dim', type=int, default=16)
+    parser.add_argument('-batch_size', type=int, default=128)
+    parser.add_argument('-batch_size_test', type=int, default=9999)
+    parser.add_argument('-lr', type=float, default=0.0001)
+    parser.add_argument('-num_layer', type=int, default=5)
+    parser.add_argument('-hidden_dim', type=int, default=16)
+    parser.add_argument('-num_trial', type=int, default=2)
+    parser.add_argument('-num_epoch', type=int, default=20)
+    parser.add_argument('-eval_freq', type=int, default=2)
+    parser.add_argument('-is_adaptive', type=int, default=1)
+    parser.add_argument('-num_cluster', type=int, default=2)
+    parser.add_argument('-alpha', type=float, default=0)
+    parser.add_argument('-n_train', type=int, default=10)
+    parser.add_argument('-dropout', type=float, default=0.3, help='Dropout rate.')
 
-if args.models is not None:
-    models = args.models.split('-')
-    print('Evaluated Baselines: ', models)
+    
+    subparsers = parser.add_subparsers()
+#     '''
+#     GraphDE parameter
+#     '''
+#     GraphDE_subparser = subparsers.add_parser('GraphDE')
+#     GraphDE_subparser.set_defaults(model='GraphDE')
+    
+    '''
+    CVTGAD parameter
+    '''
+    CVTGAD_subparser = subparsers.add_parser('CVTGAD')
+    CVTGAD_subparser.set_defaults(model='CVTGAD')
+    CVTGAD_subparser.add_argument('-GNN_Encoder', type=str, default='GIN')  
+    CVTGAD_subparser.add_argument('-graph_level_pool', type=str, default='global_mean_pool')
+    
+  
+    '''
+    GLADC parameter
+    '''
+    GLADC_subparser = subparsers.add_parser('GLADC')
+    GLADC_subparser.set_defaults(model='GLADC')
+    GLADC_subparser.add_argument('-max-nodes', dest='max_nodes', type=int, default=0,
+                        help='Maximum number of nodes (ignore graghs with nodes exceeding the number.')
+    GLADC_subparser.add_argument('-output_dim', dest='output_dim', default=128, type=int, help='Output dimension')
+    GLADC_subparser.add_argument('-nobn', dest='bn', action='store_const', const=False, default=True,
+                        help='Whether batch normalization is used')
+    GLADC_subparser.add_argument('-nobias', dest='bias', action='store_const', const=False, default=True,
+                        help='Whether to add bias. Default to True.')
 
-for dataset_name in datasets:
-    for metric in ['AUROC', 'AUPRC', 'RecK', 'Time']:
-        columns.append(dataset_name+'-'+metric)
 
-results = pandas.DataFrame(columns=columns)
-best_model_configs = {}
-file_id = None
 
-for model in models:
-    model_result = {'name': model}
-    best_model_configs[model] = {}
-    for dataset_name in datasets:
-        print('============Dataset {} Model {}=============='.format(dataset_name, model))
-        auc_list, pre_list, rec_list = [], [], []
-        set_seed()
-        time_cost = 0
-        best_val_score = 0
-        train_config = {
-            'device': args.device,
-            'epochs': 100,
-            'patience': 20,
-            'metric': 'AUPRC',
-            'inductive': False
-        }
-        data = Dataset(dataset_name)
-        data.split(args.semi_supervised, 0)
+    args = parser.parse_args()
 
-        for t in range(args.trials):
-            print("Dataset {}, Model {}, Trial {}, Time Cost {:.2f}".format(dataset_name, model, t, time_cost))
-            if time_cost > 7200:  # 86400 Stop after 1 day
-                break
-            try:
-                model_config = sample_param(model, dataset_name, t)
-                args.hidden_dim = model_config['h_feats']
-                args.num_layer = model_config['num_layers']
-                args.lr = model_config['lr']
-                args.dropout = model_config['drop_rate']
-                model = init_model(args)
-                train_config['seed'] = seed_list[t]
-                st = time.time()
-                print("model_config: ", model_config)
-                test_score = detector.train()
-                if detector.best_score > best_val_score:
-                    print("****current best score****")
-                    best_val_score = detector.best_score
-                    best_model_config = deepcopy(model_config)
-                    best_troc, best_tprc, best_treck = test_score['AUROC'], test_score['AUPRC'], test_score['RecK']
-                ed = time.time()
-                time_cost += ed - st
-                print("Current Val Best:{:.4f}; Test AUC:{:.4f}, PRC:{:.4f}, RECK:{:.4f}".format(
-                    detector.best_score, test_score['AUROC'], test_score['AUPRC'], test_score['RecK']))
-            except:
-                torch.cuda.empty_cache()
-        print("best_model_config:", best_model_config)
-        best_model_configs[model][dataset_name] = deepcopy(best_model_config)
-        model_result[dataset_name+'-AUROC'] = best_troc
-        model_result[dataset_name+'-AUPRC'] = best_tprc
-        model_result[dataset_name+'-RecK'] = best_treck
-        model_result[dataset_name+'-Time'] = time_cost/(t+1)
-    model_result = pandas.DataFrame(model_result, index=[0])
-    results = pandas.concat([results, model_result])
-    file_id = save_results(results, file_id)
+    main(args)
