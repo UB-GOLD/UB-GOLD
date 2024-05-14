34bdb28867c8cfa552bdd221bb4ba084d2a8917d
YL-wang <43315994+YL-wang@users.noreply.github.com>
2024-05-14 22:06:19 +1000

    Add files via upload

---
+++ benchmark/Source/OCGTL.sh
@@ -0,0 +1,57 @@
+python benchmark/mymain.py -exp_type ad -DS AIDS           -batch_size  128 -batch_size_test 9999 -eval_freq 2 -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4 -model OCGTL OCGTL
+
+python benchmark/mymain.py -exp_type ad -DS BZR               -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 1000 -lr 0.0001 -hidden_dim 128 -model SIGNET
+
+python benchmark/mymain.py -exp_type ad -DS COLLAB          -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 1000 -lr 0.0001 -hidden_dim 64 -model SIGNET
+
+python benchmark/mymain.py -exp_type ad -DS COX2             -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 1000 -lr 0.0001 -hidden_dim 64 -model SIGNET
+
+python benchmark/mymain.py -exp_type ad -DS DD                 -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 100 -lr 0.0001 -hidden_dim 128 -model SIGNET
+
+python benchmark/mymain.py -exp_type ad -DS DHFR             -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 1000 -lr 0.0001 -hidden_dim 128 -model SIGNET
+
+python benchmark/mymain.py -exp_type ad -DS ENZYMES           -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 1000 -lr 0.001 -hidden_dim 128 -model SIGNET
+
+python benchmark/mymain.py -exp_type ad -DS IMDB-BINARY       -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 1000 -lr 0.0001 -hidden_dim 64 -model SIGNET
+
+python benchmark/mymain.py -exp_type ad -DS NCI1              -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 1000 -lr 0.0001 -hidden_dim 128 -model SIGNET
+
+python benchmark/mymain.py -exp_type ad -DS PROTEINS_full     -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 800 -lr 0.001 -hidden_dim 16 -model SIGNET
+
+python benchmark/mymain.py -exp_type ad -DS REDDIT-BINARY     -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 1000 -lr 0.0001 -hidden_dim 128 -model SIGNET
+
+# 4 true dataset
+python benchmark/mymain.py -exp_type ad -DS Tox21_MMP           -batch_size  128 -batch_size_test 9999 -eval_freq 2 -num_epoch 4 -lr 0.001 -hidden_dim 64 -model SIGNET SIGNET
+
+python benchmark/mymain.py -exp_type ad -DS Tox21_PPAR-gamma    -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 300 -lr 0.001 -hidden_dim 64 -model SIGNET
+
+python benchmark/mymain.py -exp_type ad -DS Tox21_p53           -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 300 -lr 0.001 -hidden_dim 64 -model SIGNET
+
+python benchmark/mymain.py -exp_type ad -DS Tox21_HSE           -batch_size  128 -batch_size_test 9999 -eval_freq 2 -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4 -model OCGTL OCGTL
+
+
+# double dataset
+
+python benchmark/mymain.py -exp_type oodd -DS_pair AIDS+DHFR     -batch_size  128 -batch_size_test 9999 -eval_freq 2 -num_epoch 4 -lr 0.001 -hidden_dim 64  -model SIGNET SIGNET
+
+python benchmark/mymain.py -exp_type oodd -DS_pair BZR+COX2     -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 300 -lr 0.001 -hidden_dim 64 -model SIGNET
+
+python benchmark/mymain.py -exp_type oodd -DS_pair ENZYMES+PROTEINS     -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch  300 -lr 0.001 -hidden_dim 64  -model SIGNET
+
+python benchmark/mymain.py -exp_type oodd -DS_pair IMDB-MULTI+IMDB-BINARY     -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 300 -lr 0.001 -hidden_dim 64  -model SIGNET
+
+python benchmark/mymain.py -exp_type oodd -DS_pair PTC_MR+MUTAG     -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 300 -lr 0.001 -hidden_dim 64  -model SIGNET
+
+python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molbbbp+ogbg-molbace     -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 300 -lr 0.001 -hidden_dim 128 -model SIGNET
+
+python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molfreesolv+ogbg-moltoxcast     -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 300 -lr 0.001 -hidden_dim 128 -model SIGNET
+
+python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molclintox+ogbg-mollipo     -batch_size  128 -batch_size_test 9999 -eval_freq 5 -num_epoch 300 -lr 0.001 -hidden_dim 128 -model SIGNET
+
+python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molesol+ogbg-molmuv    -batch_size  128 -batch_size_test 9999 -num_epoch 300 -eval_freq 5 -lr 0.001 -hidden_dim 128 -model SIGNET
+
+python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-moltox21+ogbg-molsider    -batch_size  128 -batch_size_test 9999 -num_epoch 4 -eval_freq 2 -lr 0.0001 -hidden_dim 64 -model SIGNET SIGNET
+
+# drugood
+
+python benchmark/mymain.py -exp_type ood -DS DrugOOD    -batch_size  128 -batch_size_test 9999 -num_epoch 4 -lr 0.001 -eval_freq 2 -hidden_dim 16 -model  SIGNET SIGNET
