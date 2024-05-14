0e4b45238e54a29e6994907cf74990328c2afcd5
YL-wang <43315994+YL-wang@users.noreply.github.com>
2024-05-14 15:31:17 +1000

    Update __init__.py

--- GAOOD/detector/__init__.py
+++ GAOOD/detector/__init__.py
@@ -7,7 +7,8 @@ from .GraphDE import GraphDE
 from .GLADC import GLADC
 from .SIGNET import SIGNET
 from .CVTGAD import CVTGAD
+from .OCGTL import OCGTL
 
 __all__ = [
-    "Detector", "DeepDetector", "GOOD_D","GraphDE","GLocalKD", "GLADC","SIGNET", "CVTGAD"
+    "Detector", "DeepDetector", "GOOD_D","GraphDE","GLocalKD", "GLADC","SIGNET", "CVTGAD", "OCGTL"
 ]
