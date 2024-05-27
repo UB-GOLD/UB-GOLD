44a20eac0803632e64aeb229e76aad99a30381e9
YL-wang <43315994+YL-wang@users.noreply.github.com>
2024-05-27 10:39:01 +1000

    Add files via upload

---
+++ GAOOD/nn/Infograph.py
@@ -0,0 +1,64 @@
+import torch
+from torch import nn
+from torch_geometric.nn import GINConv, global_add_pool
+
+
+class GConv(nn.Module):
+    def __init__(self, input_dim, hidden_dim, activation, num_layers):
+        super(GConv, self).__init__()
+        self.activation = activation()
+        self.layers = nn.ModuleList()
+        self.batch_norms = nn.ModuleList()
+        for i in range(num_layers):
+            if i == 0:
+                self.layers.append(self.make_gin_conv(input_dim, hidden_dim))
+            else:
+                self.layers.append(self.make_gin_conv(hidden_dim, hidden_dim))
+            self.batch_norms.append(nn.BatchNorm1d(hidden_dim))
+
+    def forward(self, x, edge_index, batch):
+        z = x
+        zs = []
+        for conv, bn in zip(self.layers, self.batch_norms):
+            z = conv(z, edge_index)
+            z = self.activation(z)
+            z = bn(z)
+            zs.append(z)
+        gs = [global_add_pool(z, batch) for z in zs]
+        z, g = [torch.cat(x, dim=1) for x in [zs, gs]]
+        return z, g
+
+    def make_gin_conv(self, input_dim, out_dim):
+        return GINConv(nn.Sequential(nn.Linear(input_dim, out_dim), nn.ReLU(), nn.Linear(out_dim, out_dim)))
+
+
+class FC(nn.Module):
+    def __init__(self, hidden_dim):
+        super(FC, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU()
+        )
+        self.linear = nn.Linear(hidden_dim, hidden_dim)
+
+    def forward(self, x):
+        return self.fc(x) + self.linear(x)
+
+
+class Encoder(torch.nn.Module):
+    def __init__(self, encoder, local_fc, global_fc):
+        super(Encoder, self).__init__()
+        self.encoder = encoder
+        self.local_fc = local_fc
+        self.global_fc = global_fc
+
+    def forward(self, x, edge_index, batch):
+        z, g = self.encoder(x, edge_index, batch)
+        return z, g
+
+    def project(self, z, g):
+        return self.local_fc(z), self.global_fc(g)
---
+++ GAOOD/nn/graphcl.py
@@ -0,0 +1,64 @@
+import torch
+import os.path as osp
+import GCL.losses as L
+import GCL.augmentors as A
+import torch.nn.functional as F
+
+from torch import nn
+from tqdm import tqdm
+from torch.optim import Adam
+from GCL.eval import get_split, SVMEvaluator
+from GCL.models import DualBranchContrast
+from torch_geometric.nn import GINConv, global_add_pool
+
+
+
+
+class GConv(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers):
+        super(GConv, self).__init__()
+        self.layers = nn.ModuleList()
+        self.batch_norms = nn.ModuleList()
+
+        for i in range(num_layers):
+            if i == 0:
+                self.layers.append(self.make_gin_conv(input_dim, hidden_dim))
+            else:
+                self.layers.append(self.make_gin_conv(hidden_dim, hidden_dim))
+            self.batch_norms.append(nn.BatchNorm1d(hidden_dim))
+
+        project_dim = hidden_dim * num_layers
+        self.project = torch.nn.Sequential(
+            nn.Linear(project_dim, project_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(project_dim, project_dim))
+
+    def make_gin_conv(self, input_dim, out_dim):
+        return GINConv(nn.Sequential(nn.Linear(input_dim, out_dim), nn.ReLU(), nn.Linear(out_dim, out_dim)))
+    def forward(self, x, edge_index, batch):
+        z = x
+        zs = []
+        for conv, bn in zip(self.layers, self.batch_norms):
+            z = conv(z, edge_index)
+            z = F.relu(z)
+            z = bn(z)
+            zs.append(z)
+        gs = [global_add_pool(z, batch) for z in zs]
+        z, g = [torch.cat(x, dim=1) for x in [zs, gs]]
+        return z, g
+
+
+class Encoder(torch.nn.Module):
+    def __init__(self, encoder, augmentor):
+        super(Encoder, self).__init__()
+        self.encoder = encoder
+        self.augmentor = augmentor
+
+    def forward(self, x, edge_index, batch):
+        aug1, aug2 = self.augmentor
+        x1, edge_index1, edge_weight1 = aug1(x, edge_index)
+        x2, edge_index2, edge_weight2 = aug2(x, edge_index)
+        z, g = self.encoder(x, edge_index, batch)
+        z1, g1 = self.encoder(x1, edge_index1, batch)
+        z2, g2 = self.encoder(x2, edge_index2, batch)
+        return z, g, z1, z2, g1, g2
