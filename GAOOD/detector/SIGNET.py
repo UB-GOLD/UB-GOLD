14feca2b58e8d0654c9bf225249266a0ad2f08f6
C30cy <57309458+C30cy@users.noreply.github.com>
2024-06-03 00:08:24 +0800

    Update SIGNET.py

--- GAOOD/detector/SIGNET.py
+++ GAOOD/detector/SIGNET.py
@@ -69,8 +69,8 @@ class SIGNET(DeepDetector):
         self.device = torch.device('cuda:'+str(args.gpu) if torch.cuda.is_available() else 'cpu')
         self.model = self.init_model(**self.kwargs)
         optimizer = torch.optim.Adam(self.model.parameters(), lr=args.lr)
-        stop_counter = 0  # 初始化停止计数器
-        N = 10  # 设定阈值，比如连续5次AUC没有提升就停止
+        stop_counter = 0 
+        N = 10  
         for epoch in range(1, args.num_epoch + 1):
             self.model.train()
             loss_all = 0
@@ -101,20 +101,18 @@ class SIGNET(DeepDetector):
                 val_auc = ood_auc(ad_true, ad_score)
                 if val_auc > self.max_AUC:
                     self.max_AUC = val_auc
-                    stop_counter = 0  # 重置计数器
+                    stop_counter = 0  
                     torch.save(self.model, os.path.join(self.path, 'model_SIGNET.pth'))
                 else:
-                    stop_counter += 1  # 增加计数器
+                    stop_counter += 1  
                 print('[TRAIN] Epoch:{:03d} | val_auc:{:.4f}'.format(epoch, self.max_AUC))
                 if stop_counter >= N:
                     print(f'Early stopping triggered after {epoch} epochs due to no improvement in AUC for {N} consecutive evaluations.')
-                    break  # 达到早停条件，跳出循环
+                    break 
                 
         return True
     def is_directory_empty(self,directory):
-        # 列出目录下的所有文件和文件夹
         files_and_dirs = os.listdir(directory)
-        # 如果列表为空，则目录为空
         return len(files_and_dirs) == 0
 
     def decision_function(self, dataset, label=None, dataloader=None, args=None):
