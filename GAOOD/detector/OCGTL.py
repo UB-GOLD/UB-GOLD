fce463bd698d0b08aa246c01c65551ab6b2f7b6e
C30cy <57309458+C30cy@users.noreply.github.com>
2024-06-03 00:06:50 +0800

    Update OCGTL.py

--- GAOOD/detector/OCGTL.py
+++ GAOOD/detector/OCGTL.py
@@ -101,8 +101,8 @@ class OCGTL(DeepDetector):
         self.train_dataloader = dataloader
         self.max_AUC = 0
 
-        stop_counter = 0  # 初始化停止计数器
-        N = 10  # 设定阈值，比如连续5次AUC没有提升就停止
+        stop_counter = 0 
+        N = 10  
 
         for epoch in range(1, args.num_epoch + 1):
             
@@ -134,22 +134,21 @@ class OCGTL(DeepDetector):
                 val_auc = ood_auc(y_val, score_val)
                 if val_auc > self.max_AUC:
                     self.max_AUC = val_auc
-                    stop_counter = 0  # 重置计数器
+                    stop_counter = 0 
                     torch.save(self.model, os.path.join(self.path, 'model_OCGTL.pth'))
                 else:
-                    stop_counter += 1  # 增加计数器
+                    stop_counter += 1  
                 print('Epoch:{:03d} | val_auc:{:.4f}'.format(epoch, self.max_AUC))
                 if stop_counter >= N:
                     print(
                         f'Early stopping triggered after {epoch} epochs due to no improvement in AUC for {N} consecutive evaluations.')
-                    break  # 达到早停条件，跳出循环
+                    break 
         # self._process_decision_score()
         return self
 
     def is_directory_empty(self, directory):
-        # 列出目录下的所有文件和文件夹
+   
         files_and_dirs = os.listdir(directory)
-        # 如果列表为空，则目录为空
         return len(files_and_dirs) == 0
 
     def decision_function(self, dataset, label=None, dataloader=None, args=None):
