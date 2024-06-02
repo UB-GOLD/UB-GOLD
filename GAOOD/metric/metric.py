4ef748a9129872dc741da752196a1b8dff82b38f
se7esx <71303036+se7esx@users.noreply.github.com>
2024-06-02 16:23:21 +0800

    Update metric.py

--- GAOOD/metric/metric.py
+++ GAOOD/metric/metric.py
@@ -14,11 +14,11 @@ def ood_aupr(label,score):
     return average_precision_score(label,score)
 
 def fpr95(y_true, y_scores):
-    # 计算ROC曲线的值
+    # calculate ROC curve
     fpr, tpr, thresholds = roc_curve(y_true, y_scores)
 
-    # 寻找第一个tpr >= 0.95的索引
+    #  the index of tpr which >=0.95
     target = np.where(tpr >= 0.95)[0][0]
 
-    # 返回该点的FPR
+
     return fpr[target]
