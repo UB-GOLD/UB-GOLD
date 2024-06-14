e9cd0c0f34c6037fa8769fcf9703e356acffb878
YL-wang <43315994+YL-wang@users.noreply.github.com>
2024-06-14 16:18:22 +1000

    Update OCGTL.sh

--- benchmark/Source/OCGTL.sh
+++ benchmark/Source/OCGTL.sh
@@ -14,7 +14,7 @@ python benchmark/mymain.py -exp_type ad -DS ENZYMES          -batch_size  128 -b
 
 python benchmark/mymain.py -exp_type ad -DS IMDB-BINARY       -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.0001 -hidden_dim 64 -num_layer 4 -model OCGTL OCGTL
 
-python benchmark/mymain.py -exp_type ad -DS NCI1              -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 64 -num_layer 4 -model OCGTL OCGTL
+python benchmark/mymain.py -exp_type ad -DS NCI1              -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 128 -num_layer 4 -model OCGTL OCGTL
 
 python benchmark/mymain.py -exp_type ad -DS PROTEINS_full     -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4 -model OCGTL OCGTL
 
@@ -27,12 +27,12 @@ python benchmark/mymain.py -exp_type ad -DS Tox21_PPAR-gamma    -batch_size  128
 
 python benchmark/mymain.py -exp_type ad -DS Tox21_p53           -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4 -model OCGTL OCGTL
 
-python benchmark/mymain.py -exp_type ad -DS Tox21_HSE           -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 64 -num_layer 4 -model OCGTL OCGTL
+python benchmark/mymain.py -exp_type ad -DS Tox21_HSE           -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.0001 -hidden_dim 64 -num_layer 4 -model OCGTL OCGTL
 
 
 # double dataset
 
-python benchmark/mymain.py -exp_type oodd -DS_pair AIDS+DHFR    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 3 -model OCGTL OCGTL
+python benchmark/mymain.py -exp_type oodd -DS_pair AIDS+DHFR    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 128 -num_layer 3 -model OCGTL OCGTL
 
 python benchmark/mymain.py -exp_type oodd -DS_pair BZR+COX2     -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.0001 -hidden_dim 32 -num_layer 4 -model OCGTL OCGTL
 
@@ -42,15 +42,15 @@ python benchmark/mymain.py -exp_type oodd -DS_pair IMDB-MULTI+IMDB-BINARY     -b
 
 python benchmark/mymain.py -exp_type oodd -DS_pair PTC_MR+MUTAG     -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.0001 -hidden_dim 64 -num_layer 3 -model OCGTL OCGTL
 
-python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molbbbp+ogbg-molbace     -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 3 -model OCGTL OCGTL
+python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molbbbp+ogbg-molbace     -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.0001 -hidden_dim 32 -num_layer 3 -model OCGTL OCGTL
 
-python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molfreesolv+ogbg-moltoxcast    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4 -model OCGTL OCGTL
+python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molfreesolv+ogbg-moltoxcast    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 128 -num_layer 4 -model OCGTL OCGTL
 
-python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molclintox+ogbg-mollipo    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4 -model OCGTL OCGTL
+python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molclintox+ogbg-mollipo    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 128 -num_layer 4 -model OCGTL OCGTL
 
 python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molesol+ogbg-molmuv    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4 -model OCGTL OCGTL
 
-python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-moltox21+ogbg-molsider    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 64 -num_layer 3 -model OCGTL OCGTL
+python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-moltox21+ogbg-molsider    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.0001 -hidden_dim 64 -num_layer 3 -model OCGTL OCGTL
 
 
 
