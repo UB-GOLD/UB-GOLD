3fa6e8e69af2881d7802572d13e27a0967454c3d
YL-wang <43315994+YL-wang@users.noreply.github.com>
2024-06-17 18:52:40 +1000

    Update GOOD-D.sh

--- benchmark/Source/GOOD-D.sh
+++ benchmark/Source/GOOD-D.sh
@@ -4,13 +4,13 @@ python benchmark/mymain.py -exp_type ad -DS AIDS -num_epoch 400 -num_cluster 3 -
 
 python benchmark/mymain.py -exp_type ad -DS BZR -num_epoch 400 -num_cluster 2 -alpha 0.8 -model GOOD-D
 
-python benchmark/mymain.py -exp_type ad -DS COLLAB -batch_size 64 -batch_size_test 64 -num_epoch 100 -num_cluster 2 -alpha 0.8 -model GOOD-D
+python benchmark/mymain.py -exp_type ad -DS COLLAB -batch_size 64 -batch_size_test 64 -num_epoch 400 -num_cluster 2 -alpha 0.8 -model GOOD-D
 
-python benchmark/mymain.py  -exp_type ad -DS COX2 -num_epoch 150 -num_cluster 3 -alpha 0.4 -model GOOD-D
+python benchmark/mymain.py  -exp_type ad -DS COX2 -num_epoch 400 -num_cluster 3 -alpha 0.4 -model GOOD-D
 
 python benchmark/mymain.py  -exp_type ad -DS DD -batch_size 16 -batch_size_test 16 -num_epoch 50 -num_cluster 2 -alpha 1.0 -model GOOD-D
 
-python benchmark/mymain.py -exp_type ad -DS DHFR -num_epoch 20 -num_cluster 2 -alpha 0 -model GOOD-D
+python benchmark/mymain.py -exp_type ad -DS DHFR -num_epoch 400 -num_cluster 2 -alpha 0 -model GOOD-D
 
 python benchmark/mymain.py  -exp_type ad -DS ENZYMES -num_epoch 400 -num_cluster 10 -alpha 0.2 -model GOOD-D
 
@@ -18,16 +18,16 @@ python benchmark/mymain.py  -exp_type ad -DS IMDB-BINARY -num_epoch 400 -num_clu
 
 python benchmark/mymain.py  -exp_type ad -DS NCI1 -batch_size 64 -batch_size_test 64 -num_epoch 400 -num_cluster 20 -alpha 1.0 -model GOOD-D
 
-python benchmark/mymain.py  -exp_type ad -DS PROTEINS_full -num_epoch 20 -num_cluster 2 -alpha 0.2 -model GOOD-D
+python benchmark/mymain.py  -exp_type ad -DS PROTEINS_full -num_epoch 400 -num_cluster 2 -alpha 0.2 -model GOOD-D
 
 python benchmark/mymain.py  -exp_type ad -DS REDDIT-BINARY -batch_size 16 -batch_size_test 16 -num_epoch 80 -num_cluster 30 -alpha 0.8 -model GOOD-D
 
 # 4 true dataset
 python benchmark/mymain.py  -exp_type ad -DS Tox21_MMP -num_epoch 400 -num_cluster 5 -alpha 0.0 -model GOOD-D
 
-python benchmark/mymain.py  -exp_type ad -DS Tox21_PPAR-gamma -num_epoch 200 -num_cluster 10 -alpha 0.8 -model GOOD-D
+python benchmark/mymain.py  -exp_type ad -DS Tox21_PPAR-gamma -num_epoch 4000 -num_cluster 10 -alpha 0.8 -model GOOD-D
 
-python benchmark/mymain.py  -exp_type ad -DS Tox21_p53 -num_epoch 150 -num_cluster 5 -alpha 0.2 -model GOOD-D
+python benchmark/mymain.py  -exp_type ad -DS Tox21_p53 -num_epoch 400 -num_cluster 5 -alpha 0.2 -model GOOD-D
 
 python benchmark/mymain.py  -exp_type ad -DS Tox21_HSE -num_epoch 300 -num_cluster 2 -alpha 0.2 -model GOOD-D
 
@@ -38,9 +38,9 @@ python benchmark/mymain.py -exp_type oodd -DS_pair AIDS+DHFR -batch_size_test 12
 
 python benchmark/mymain.py -exp_type oodd -DS_pair BZR+COX2 -num_epoch 400 -num_cluster 2 -alpha 0 -model GOOD-D
 
-python benchmark/mymain.py -exp_type oodd -DS_pair ENZYMES+PROTEINS -num_epoch 150 -num_cluster 15 -alpha 0.2 -model GOOD-D
+python benchmark/mymain.py -exp_type oodd -DS_pair ENZYMES+PROTEINS -num_epoch 400 -num_cluster 15 -alpha 0.2 -model GOOD-D
 
-python benchmark/mymain.py -exp_type oodd -DS_pair IMDB-MULTI+IMDB-BINARY -num_epoch 20 -num_cluster 5 -alpha 0.8 -model GOOD-D
+python benchmark/mymain.py -exp_type oodd -DS_pair IMDB-MULTI+IMDB-BINARY -num_epoch 400 -num_cluster 5 -alpha 0.8 -model GOOD-D
 
 python benchmark/mymain.py -exp_type oodd -DS_pair PTC_MR+MUTAG -num_epoch 400 -num_cluster 2 -alpha 0.8 -model GOOD-D
 
@@ -56,6 +56,6 @@ python benchmark/mymain.py -exp_type oodd -DS_pair ogbg-moltox21+ogbg-molsider -
 
 # drugood
 
-python benchmark/mymain.py -exp_type ood -DS DrugOOD -num_epoch 200 -num_cluster 10 -alpha 0.2 -model GOOD-D
+python benchmark/mymain.py -exp_type ood -DS DrugOOD -num_epoch 4000 -num_cluster 10 -alpha 0.2 -model GOOD-D
 
 
