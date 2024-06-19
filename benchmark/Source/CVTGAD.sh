e6f0819a8376ddeb0f9a66a13b4a86f63ef87556
YL-wang <43315994+YL-wang@users.noreply.github.com>
2024-06-19 18:04:12 +1000

    Update CVTGAD.sh

--- benchmark/Source/CVTGAD.sh
+++ benchmark/Source/CVTGAD.sh
@@ -20,7 +20,7 @@ python3 benchmark/mymain.py -exp_type ad -DS NCI1           -batch_size 64 -batc
 
 python3 benchmark/mymain.py -exp_type ad -DS PROTEINS_full  -num_epoch 600 -num_cluster 2 -alpha 0.2 -num_layer 5  -model CVTGAD CVTGAD -GNN_Encoder GCN -graph_level_pool global_mean_pool
 
-python3 benchmark/mymain.py -exp_type ad -DS REDDIT-BINARY  -rw_dim 8 -dg_dim 8 -hidden_dim 16 -batch_size 16 -batch_size_test 16 -num_epoch 800 -num_cluster 30 -alpha 0.2 -num_layer 6  -model CVTGAD CVTGAD -GNN_Encoder GCN -graph_level_pool global_mean_pool
+python3 benchmark/mymain.py -exp_type ad -DS REDDIT-BINARY  -rw_dim 8 -dg_dim 8 -hidden_dim 16 -batch_size 16 -batch_size_test 16 -num_epoch 800 -num_cluster 30 -alpha 0.2 -num_layer 5  -model CVTGAD CVTGAD -GNN_Encoder GCN -graph_level_pool global_mean_pool
 
 # 4 true dataset
 python3 benchmark/mymain.py -exp_type ad -DS Tox21_MMP        -rw_dim 16 -dg_dim 16 -hidden_dim 8 -num_epoch 600 -num_cluster 5 -alpha 0.42 -num_layer 5  -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
@@ -29,28 +29,28 @@ python3 benchmark/mymain.py -exp_type ad -DS Tox21_PPAR-gamma -rw_dim 4 -dg_dim 
 
 python3 benchmark/mymain.py -exp_type ad -DS Tox21_p53        -rw_dim 12 -dg_dim 12 -hidden_dim 24 -num_epoch 400 -num_cluster 5 -alpha 0.2 -num_layer 5  -model CVTGAD CVTGAD -GNN_Encoder GCN -graph_level_pool global_mean_pool
 
-python3 benchmark/mymain.py -exp_type ad -DS Tox21_HSE        -rw_dim 4 -dg_dim 4 -hidden_dim 4 -num_epoch 400 -num_cluster 2 -alpha 0.32 -num_layer 8  -model CVTGAD CVTGAD -GNN_Encoder GCN -graph_level_pool global_mean_pool
+python3 benchmark/mymain.py -exp_type ad -DS Tox21_HSE        -rw_dim 4 -dg_dim 4 -hidden_dim 4 -num_epoch 400 -num_cluster 2 -alpha 0.32 -num_layer 5  -model CVTGAD CVTGAD -GNN_Encoder GCN -graph_level_pool global_mean_pool
 
 
 # double dataset
 
-python3 benchmark/mymain.py -exp_type oodd -DS_pair AIDS+DHFR -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
+python3 benchmark/mymain.py -exp_type oodd -DS_pair AIDS+DHFR -rw_dim 8 -dg_dim 8 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
-python3 benchmark/mymain.py -exp_type oodd -DS_pair BZR+COX2 -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.32 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
+python3 benchmark/mymain.py -exp_type oodd -DS_pair BZR+COX2 -rw_dim 8 -dg_dim 8 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.32 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
-python3 benchmark/mymain.py -exp_type oodd -DS_pair ENZYMES+PROTEINS -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
+python3 benchmark/mymain.py -exp_type oodd -DS_pair ENZYMES+PROTEINS -rw_dim 8 -dg_dim 8 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
 python3 benchmark/mymain.py -exp_type oodd -DS_pair IMDB-MULTI+IMDB-BINARY -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
-python3 benchmark/mymain.py -exp_type oodd -DS_pair PTC_MR+MUTAG -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.0 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
+python3 benchmark/mymain.py -exp_type oodd -DS_pair PTC_MR+MUTAG -rw_dim 8 -dg_dim 8 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.0 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
-python3 benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molbbbp+ogbg-molbace -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.32 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
+python3 benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molbbbp+ogbg-molbace -rw_dim 8 -dg_dim 8 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.32 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
 python3 benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molfreesolv+ogbg-moltoxcast -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
-python3 benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molclintox+ogbg-mollipo -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
+python3 benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molclintox+ogbg-mollipo -rw_dim 8 -dg_dim 8 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
-python3 benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molesol+ogbg-molmuv -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 3  -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
+python3 benchmark/mymain.py -exp_type oodd -DS_pair ogbg-molesol+ogbg-molmuv -rw_dim 8 -dg_dim 8 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 3  -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
 python3 benchmark/mymain.py -exp_type oodd -DS_pair ogbg-moltox21+ogbg-molsider -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 3  -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
