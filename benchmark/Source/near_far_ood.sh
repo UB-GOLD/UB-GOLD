6fc79207779c28d9fb372ae205249d8e77b8acf6
YL-wang <43315994+YL-wang@users.noreply.github.com>
2024-06-20 23:00:41 +1000

    Update near_far_ood.sh

--- benchmark/Source/near_far_ood.sh
+++ benchmark/Source/near_far_ood.sh
@@ -3,45 +3,48 @@
 # ENZYMES
 
 #CVTGOD
-python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.0 -num_layer 3   -near far -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
+python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR -rw_dim 8 -dg_dim 8 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 5   -lr 0.0001    -near far -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
-python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2 -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.0 -num_layer 3  -near far -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
+python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2 -rw_dim 8 -dg_dim 8 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.32 -num_layer 5   -lr 0.001   -near far -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
-python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS  -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.0 -num_layer 3 -near far   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
+python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS  -rw_dim 8 -dg_dim 8 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 3   -lr 0.0001 -near far   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
-python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.0 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
+python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR -rw_dim 8 -dg_dim 8 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 5   -lr 0.0001    -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
-python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2 -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.0 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
+python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2 -rw_dim 8 -dg_dim 8 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.32 -num_layer 5   -lr 0.001   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
 
-python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS  -rw_dim 16 -dg_dim 16 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.0 -num_layer 3   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
+python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS  -rw_dim 8 -dg_dim 8 -hidden_dim 16 -num_epoch 300 -num_cluster 2 -alpha 0.2 -num_layer 3   -lr 0.0001   -model CVTGAD CVTGAD -GNN_Encoder GIN -graph_level_pool global_mean_pool
+
 
 
 
 #GLADC
-python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR -num_epoch 100  -batch_size 300 -batch_size_test 1 -hidden_dim 256  -num_layer 2 -dropout 0.1  -lr 0.0001 -near far -model GLADC GLADC
+python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR -num_epoch 100  -batch_size 300 -batch_size_test 1 -hidden_dim 128   -dropout 0.1 -near far -lr 0.001 -model GLADC GLADC -output_dim 64
 
-python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2  -num_epoch 100  -batch_size 300 -batch_size_test 1 -hidden_dim 256  -num_layer 2 -dropout 0.1  -lr 0.0001  -near far -model GLADC GLADC
+python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2  -num_epoch 100  -batch_size 300 -batch_size_test 1  -hidden_dim 128   -dropout 0.1 -near far -lr 0.001 -model GLADC GLADC -output_dim 64
 
-python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS -num_epoch 100  -batch_size 300 -batch_size_test 1 -hidden_dim 256  -num_layer 2 -dropout 0.1  -lr 0.0001  -near far -model GLADC GLADC
+python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS  -num_epoch 100  -batch_size 300 -batch_size_test 1 -hidden_dim 64   -dropout 0.1 -near far -lr 0.0001 -model GLADC GLADC -output_dim 32
 
-python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR -num_epoch 100  -batch_size 300 -batch_size_test 1 -hidden_dim 256  -num_layer 2 -dropout 0.1  -lr 0.0001 -model GLADC GLADC
+python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR -num_epoch 100  -batch_size 300 -batch_size_test 1 -hidden_dim 128   -dropout 0.1  -lr 0.001 -model GLADC GLADC -output_dim 64
 
-python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2  -num_epoch 100  -batch_size 300 -batch_size_test 1 -hidden_dim 256  -num_layer 2 -dropout 0.1  -lr 0.0001 -model GLADC GLADC
+python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2  -num_epoch 100  -batch_size 300 -batch_size_test 1  -hidden_dim 128   -dropout 0.1  -lr 0.001 -model GLADC GLADC -output_dim 64
 
-python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS -num_epoch 100  -batch_size 300 -batch_size_test 1 -hidden_dim 256  -num_layer 2 -dropout 0.1  -lr 0.0001 -model GLADC GLADC
+python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS  -num_epoch 100  -batch_size 300 -batch_size_test 1 -hidden_dim 64   -dropout 0.1  -lr 0.0001 -model GLADC GLADC -output_dim 32
+
+
 
 #GLocalKD
-python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR  -num_epoch 50  -batch_size 300 -batch_size_test 1 -hidden_dim 32 -num_layer 3   -near far -model GLocalKD GLocalKD -max-nodes 0
+python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR   -num_epoch 150  -batch_size 300 -batch_size_test 1 -hidden_dim 32 -lr 0.0001 -num_layer 3  -near far -model GLocalKD GLocalKD -output_dim 256
 
-python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2   -num_epoch 50  -batch_size 300 -batch_size_test 1 -hidden_dim 32 -num_layer 3   -near far -model GLocalKD GLocalKD -max-nodes 0
+python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2   -num_epoch 150  -batch_size 300 -batch_size_test 1 -hidden_dim 64 -num_layer 4  -near far -model GLocalKD GLocalKD -output_dim 128
+python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS -num_epoch 150  -batch_size 300 -batch_size_test 1 -hidden_dim 32 -lr 0.001 -num_layer 3  -near far -model GLocalKD GLocalKD -output_dim 256
 
-python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS -exp_type oodd -DS_pair ENZYMES+PROTEINS  -num_epoch 50  -batch_size 300 -batch_size_test 1 -hidden_dim 32 -num_layer 3   -near far -model GLocalKD GLocalKD -max-nodes 0
 
-python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR  -num_epoch 50  -batch_size 300 -batch_size_test 1 -hidden_dim 32 -num_layer 3  -model GLocalKD GLocalKD -max-nodes 0
+python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR   -num_epoch 150  -batch_size 300 -batch_size_test 1 -hidden_dim 32 -lr 0.0001 -num_layer 3  -model GLocalKD GLocalKD -output_dim 256
 
-python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2   -num_epoch 50  -batch_size 300 -batch_size_test 1 -hidden_dim 32 -num_layer 3  -model GLocalKD GLocalKD -max-nodes 0
+python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2   -num_epoch 150  -batch_size 300 -batch_size_test 1 -hidden_dim 64 -num_layer 4  -model GLocalKD GLocalKD -output_dim 128
+python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS -num_epoch 150  -batch_size 300 -batch_size_test 1 -hidden_dim 32 -lr 0.001 -num_layer 3  -model GLocalKD GLocalKD -output_dim 256
 
-python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS -exp_type oodd -DS_pair ENZYMES+PROTEINS  -num_epoch 50  -batch_size 300 -batch_size_test 1 -hidden_dim 32 -num_layer 3  -model GLocalKD GLocalKD -max-nodes 0
 
 #GOOD-D
 python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR  -batch_size_test 128 -num_epoch 400 -num_cluster 10 -alpha 0.2  -near far -model GOOD-D
@@ -56,7 +59,7 @@ python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2  -num_e
 
 python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS -num_epoch 150 -num_cluster 15 -alpha 0.2 -model GOOD-D
 
-#GraphDE
+# #GraphDE
 
 python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR -batch_size 64 -batch_size_test 128 -num_epoch 400 -alpha 0.2  -near far -model GraphDE -num_layer 2 -hidden_dim 64
 
@@ -71,48 +74,48 @@ python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2  -batch
 python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS -batch_size 64 -num_epoch 150  -alpha 0.2 -model GraphDE -num_layer 2 -hidden_dim 64
 
 # OCGIN
-python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR     -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4  -near far -model OCGIN OCGIN
+python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR      -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4  -near far -model OCGIN OCGIN
 
-python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2     -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4  -near far -model OCGIN OCGIN
+python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2     -batch_size  128 -batch_size_test 9999   -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.0001 -hidden_dim 32 -num_layer 4 -near far -model OCGIN OCGIN
 
 python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4  -near far -model OCGIN OCGIN
 
 python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR     -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4 -model OCGIN OCGIN
 
-python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2     -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4 -model OCGIN OCGIN
+python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2      -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.0001 -hidden_dim 32 -num_layer 4 -model OCGIN OCGIN
 
 python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4 -model OCGIN OCGIN
 
 # OCGTL
-python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4  -near far -model OCGTL OCGTL
+python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 128 -num_layer 3 -near far -model OCGTL OCGTL
 
-python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2      -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4  -near far -model OCGTL OCGTL
+python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2       -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.0001 -hidden_dim 32 -num_layer 4  -near far -model OCGTL OCGTL
 
-python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS   -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4  -near far -model OCGTL OCGTL
+python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS   -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 64 -num_layer 3  -near far -model OCGTL OCGTL
 
-python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4 -model OCGTL OCGTL
+python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 128 -num_layer 3 -model OCGTL OCGTL
 
-python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2      -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4 -model OCGTL OCGTL
+python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2      -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.0001 -hidden_dim 32 -num_layer 4 -model OCGTL OCGTL
 
-python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS   -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 32 -num_layer 4 -model OCGTL OCGTL
+python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS    -batch_size  128 -batch_size_test 9999  -num_epoch 500 -lr 0.001 -hidden_dim 64 -num_layer 3 -model OCGTL OCGTL
 
 # SIGNET
 
-python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR     -batch_size  128 -batch_size_test 9999  -num_epoch 300 -lr 0.001 -hidden_dim 64   -near far -model SIGNET SIGNET
+python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR      -batch_size  128 -batch_size_test 9999  -num_epoch 300 -lr 0.0001 -hidden_dim 64 -near far -model SIGNET SIGNET  -readout concat  -encoder_layers 3
 
-python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2    -batch_size  128 -batch_size_test 9999  -num_epoch 300 -lr 0.001 -hidden_dim 64  -near far -model SIGNET SIGNET
+python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2    -batch_size  128 -batch_size_test 9999  -num_epoch 300 -lr 0.001 -hidden_dim 64 -near far -model SIGNET SIGNET  -readout concat  -encoder_layers 3
 
-python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS     -batch_size  128 -batch_size_test 9999  -num_epoch  300 -lr 0.001 -hidden_dim 64   -near far -model SIGNET SIGNET
+python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS    -batch_size  128 -batch_size_test 9999  -num_epoch  300 -lr 0.0001 -hidden_dim 64 -near far -model SIGNET SIGNET  -readout concat  -encoder_layers 5
 
-python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR     -batch_size  128 -batch_size_test 9999  -num_epoch 300 -lr 0.001 -hidden_dim 64  -model SIGNET SIGNET
+python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR      -batch_size  128 -batch_size_test 9999  -num_epoch 300 -lr 0.0001 -hidden_dim 64  -model SIGNET SIGNET  -readout concat  -encoder_layers 3
 
-python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2    -batch_size  128 -batch_size_test 9999  -num_epoch 300 -lr 0.001 -hidden_dim 64 -model SIGNET SIGNET
+python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2    -batch_size  128 -batch_size_test 9999  -num_epoch 300 -lr 0.001 -hidden_dim 64 -model SIGNET SIGNET  -readout concat  -encoder_layers 3
 
-python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS     -batch_size  128 -batch_size_test 9999  -num_epoch  300 -lr 0.001 -hidden_dim 64  -model SIGNET SIGNET
+python benchmark/near_far_ood.py -exp_type ood -DS ENZYMES -DS_pair ENZYMES+PROTEINS    -batch_size  128 -batch_size_test 9999  -num_epoch  300 -lr 0.0001 -hidden_dim 64  -model SIGNET SIGNET  -readout concat  -encoder_layers 5
 
 
 
-# GCL-IF
+# # GCL-IF
 python benchmark/near_far_ood.py -exp_type ood -DS AIDS -DS_pair AIDS+DHFR   -near far -batch_size  128 -batch_size_test 9999  -num_epoch 30  -model GraphCL_IF GraphCL_IF -detector IF -IF_n_trees 200  -IF_sample_ratio 0.5
 
 python benchmark/near_far_ood.py -exp_type ood -DS BZR -DS_pair BZR+COX2    -near far -batch_size  128 -batch_size_test 9999  -num_epoch 30  -model GraphCL_IF GraphCL_IF -detector IF -IF_n_trees 200  -IF_sample_ratio 0.5
